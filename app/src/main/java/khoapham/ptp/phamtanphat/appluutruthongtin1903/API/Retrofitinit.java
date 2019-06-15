77aed1e408716fcf2a081ee9356593c08635f8c5
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-15 13:46:29 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/API/Retrofitinit.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/API/Retrofitinit.java
@@ -1,5 +1,8 @@
 package khoapham.ptp.phamtanphat.appluutruthongtin1903.API;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.sql.Time;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
@@ -7,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import okhttp3.Protocol;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Retrofitinit  {
     private static Retrofit retrofit = null;
@@ -19,5 +23,14 @@ public class Retrofitinit  {
                                 .retryOnConnectionFailure(true)
                                 .protocols(Arrays.asList(Protocol.HTTP_1_1))
                                 .build();
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        retrofit = new Retrofit.Builder()
+                    .baseUrl(base_url)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        return  retrofit;
+
     }
 }
