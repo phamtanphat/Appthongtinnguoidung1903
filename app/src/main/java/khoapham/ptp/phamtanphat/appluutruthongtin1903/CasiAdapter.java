54f25cb4311faf72896373cbae52435b8e3c2964
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-15 14:28:30 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/CasiAdapter.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/CasiAdapter.java
@@ -2,14 +2,47 @@ package khoapham.ptp.phamtanphat.appluutruthongtin1903;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import khoapham.ptp.phamtanphat.appluutruthongtin1903.API.model.Singer;
 
 public class CasiAdapter extends ArrayAdapter<Singer> {
+    ArrayList<Singer> mang;
     public CasiAdapter( @NonNull Context context, int resource,@NonNull List<Singer> objects) {
         super(context, resource, objects);
+        mang = (ArrayList<Singer>) objects;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position,@Nullable View convertView,@NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+        convertView = layoutInflater.inflate(R.layout.dong_casi_item,null);
+        TextView txtTen = convertView.findViewById(R.id.textviewTencasi);
+        TextView txtEmail = convertView.findViewById(R.id.textviewEmail);
+        ImageView img = convertView.findViewById(R.id.imageviewCasi);
+
+        Singer singer = getItem(position);
+
+        txtTen.setText(singer.getName());
+        txtEmail.setText(singer.getEmail());
+        Glide.with(getContext()).load(singer.getAvatar()).into(img);
+        return convertView;
+    }
+    public void moreData(ArrayList<Singer> mangdulieu){
+        mang.addAll(mangdulieu);
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/MainActivity.java
@@ -1,30 +1,65 @@
 package khoapham.ptp.phamtanphat.appluutruthongtin1903;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import khoapham.ptp.phamtanphat.appluutruthongtin1903.API.APICallback;
 import khoapham.ptp.phamtanphat.appluutruthongtin1903.API.DataResponse;
 import khoapham.ptp.phamtanphat.appluutruthongtin1903.API.model.CasiAPI;
+import khoapham.ptp.phamtanphat.appluutruthongtin1903.API.model.Singer;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView lvCasi;
+    CasiAdapter casiAdapter;
+    Button btnLichsu;
+    SwipeRefreshLayout swipeRefreshLayout;
+    ArrayList<Singer> mangdulieu = new ArrayList<>();
+    boolean checkRefresh = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lvCasi = findViewById(R.id.listviewCasi);
+        btnLichsu=findViewById(R.id.buttonThongtin);
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+
+        casiAdapter = new CasiAdapter(MainActivity.this,android.R.layout.simple_list_item_1,mangdulieu);
+        lvCasi.setAdapter(casiAdapter);
+        getCasi();
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                getCasi();
+                if (checkRefresh == true){
+                    swipeRefreshLayout.setRefreshing(false);
+                    checkRefresh = false;
+                }
+            }
+        });
+    }
+    private void getCasi(){
         APICallback apiCallback = DataResponse.sendRequest();
         Call<CasiAPI> callback = apiCallback.getCasi();
         callback.enqueue(new Callback<CasiAPI>() {
             @Override
             public void onResponse(Call<CasiAPI> call, Response<CasiAPI> response) {
                 CasiAPI casiAPI = response.body();
-                Log.d("BBB",casiAPI.getSingers().get(0).getAvatar());
+                ArrayList<Singer> mangcasi = (ArrayList<Singer>) casiAPI.getSingers();
+                casiAdapter.moreData(mangcasi);
+                casiAdapter.notifyDataSetChanged();
+                checkRefresh = true;
             }
 
             @Override
