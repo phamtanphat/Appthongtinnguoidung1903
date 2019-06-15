47f113f468e00167d30f299165e29caacc13659f
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-15 13:58:57 +0700

    A

--- app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/API/APICallback.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/API/APICallback.java
@@ -1,4 +1,10 @@
 package khoapham.ptp.phamtanphat.appluutruthongtin1903.API;
 
+import khoapham.ptp.phamtanphat.appluutruthongtin1903.API.model.CasiAPI;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface APICallback {
+    @GET("singer")
+    Call<CasiAPI> getCasi();
 }
--- app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/MainActivity.java
@@ -2,6 +2,14 @@ package khoapham.ptp.phamtanphat.appluutruthongtin1903;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import khoapham.ptp.phamtanphat.appluutruthongtin1903.API.APICallback;
+import khoapham.ptp.phamtanphat.appluutruthongtin1903.API.DataResponse;
+import khoapham.ptp.phamtanphat.appluutruthongtin1903.API.model.CasiAPI;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        APICallback apiCallback = DataResponse.sendRequest();
+        Call<CasiAPI> callback = apiCallback.getCasi();
+        callback.enqueue(new Callback<CasiAPI>() {
+            @Override
+            public void onResponse(Call<CasiAPI> call, Response<CasiAPI> response) {
+                CasiAPI casiAPI = response.body();
+                Log.d("BBB",casiAPI.getSingers().get(0).getAvatar());
+            }
+
+            @Override
+            public void onFailure(Call<CasiAPI> call, Throwable t) {
+
+            }
+        });
     }
 }
