9ab0f6e7ee0569383167426cba8d69365540b294
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-15 13:50:13 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/API/DataResponse.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/appluutruthongtin1903/API/DataResponse.java
@@ -1,4 +1,9 @@
 package khoapham.ptp.phamtanphat.appluutruthongtin1903.API;
 
 public class DataResponse {
+    private static final String base_url = "https://servertest12.herokuapp.com/";
+
+    public static APICallback sendRequest(){
+        return Retrofitinit.initRetro(base_url).create(APICallback.class);
+    }
 }
